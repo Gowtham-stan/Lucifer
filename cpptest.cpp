58ec14f9b9e3c9309d8ebce76530e8ee8ade16b6
Gowtham-stan <59704139+Gowtham-stan@users.noreply.github.com>
2020-01-10 00:05:56 +0530

    Create cpptest.cpp

---
+++ cpptest.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+class ClassC {
+public:
+  void printHello() {
+    cout << "hello world";
+  }
+};
+
+class ClassB {
+public:
+  ClassC* C;
+  ClassB() {
+    C = new ClassC();
+  }
+};
+
+class ClassA {
+public:
+  ClassB* B;
+  ClassA() {
+    // Uncomment to fix
+    // B = new ClassB();
+  }
+};
+
+int main() {
+  ClassA* A = new ClassA();
+  A->B->C->printHello();
+
+}
