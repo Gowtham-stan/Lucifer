1c4c0e2f2e83be02963424a59b540d7222989f1d
Gowtham-stan <59704139+Gowtham-stan@users.noreply.github.com>
2020-01-09 23:39:55 +0530

    Add files via upload

---
+++ Hospital.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+class Hospital
+{
+public static void main(String args[])
+{
+int n,c,atime;
+String name;
+System.out.println("Enter the number of patients");
+Scanner sc=new Scanner(System.in);
+n=sc.nextInt();
+for(int i=0;i<n;i++)
+{
+System.out.println("Enter the Name of patient");
+name=sc.next();
+System.out.println("Enter consultation time");
+atime=sc.nextInt();
+System.out.println("Choose the patient's category: \n 1.Woman \n 2.Children \n 3.Elder \n 4.Youngster");
+c=sc.nextInt();
+switch(c)
+{
+
+}
+}
+}
+
+}
